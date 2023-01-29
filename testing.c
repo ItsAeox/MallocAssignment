ef2d0d9761edf6d3a0aef0331a8353f417d33f6f
ItsAeox <106143446+ItsAeox@users.noreply.github.com>
2023-01-29 13:39:59 +0530

    Update testing.c

--- testing.c
+++ testing.c
@@ -6,25 +6,25 @@
 
 int memory[MEM_SIZE];
 
+typedef struct mem_block{
+    size_t size;
+    int free;
+    mem_block *next;
+}mem_block;
 
+mem_block free_mem;
 
-int check_free(int size){
-    int temp = size;
-    for(int i = 0; i < MEM_SIZE; i++){
+mem_block *start = (mem_block*)memory;
 
-    }
+void ini_mem(){
+    free_mem.size = 25000 - sizeof(mem_block);
+    free_mem.free = 1;
+    free_mem.next = NULL;  
 }
 
-int* MyMalloc(int size){
-    
-}
-
-int main(){
-    for(int i = 0; i < MEM_SIZE; i++){
-        memory[i] = 0;
+void* MyMalloc(size_t size){
+    if(!(free_mem.size)){
+        ini_mem();
+        printf("Memory Initialization Succesful!");
     }
-
-
-
-    return 0;
 }
