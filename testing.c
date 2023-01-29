c29f7733269cacfe0ef2abe58b5e76ae2f4368fa
ItsAeox <106143446+ItsAeox@users.noreply.github.com>
2023-01-29 15:56:31 +0530

    added mem allocation to an empty memory
    searching for a suitable memory slot is yet to be implemented

--- testing.c
+++ testing.c
@@ -4,27 +4,50 @@
 
 #define MEM_SIZE 25000
 
-int memory[MEM_SIZE];
+char memory[MEM_SIZE];
 
 typedef struct mem_block{
     size_t size;
     int free;
-    mem_block *next;
+    struct mem_block *next;
 }mem_block;
 
-mem_block free_mem;
+mem_block *free_mem = (void*)memory;
 
 mem_block *start = (mem_block*)memory;
 
-void ini_mem(){
-    free_mem.size = 25000 - sizeof(mem_block);
-    free_mem.free = 1;
-    free_mem.next = NULL;  
-}
-
 void* MyMalloc(size_t size){
-    if(!(free_mem.size)){
-        ini_mem();
-        printf("Memory Initialization Succesful!");
+
+    mem_block *temp;
+    void *output;
+
+    if(!(free_mem->size)){
+        free_mem->size = 25000;
+        free_mem->free = 1;
+        free_mem->next = NULL;  
+        printf("\nMemory Initialization Succesful!");
+    }
+
+    temp = free_mem;
+
+    if(temp->size < (size + sizeof(mem_block))){
+        printf("\nError! Memory Allocation Unsuccesful. Not Enough Memory Available.");
+        output = NULL;
+        return output;
+    }
+    else if(temp->size == (size + sizeof(mem_block))){
+        temp->free = 0;
+        output = (void*)(temp + sizeof(mem_block));
+        printf("\nMemory Allocation Succesful! No Memory Left In The Heap.");
+        return output;
+    }
+    else{
+        mem_block *temp_2 = (void*)(temp+size+sizeof(mem_block));
+        temp->free = 0;
+        temp->size = size + sizeof(mem_block);
+        temp->next = temp_2;
+        output = (void*)(temp +sizeof(mem_block));
+        printf("\nMemory Allocation Succesful!");
+        return output;
     }
 }
