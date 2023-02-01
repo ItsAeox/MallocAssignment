211a76cbc110d1ef4a11bc1ea20eb6a0db66698e
ItsAeox <106143446+ItsAeox@users.noreply.github.com>
2023-02-01 14:43:01 +0530

    Allocation implementation complete
    myfree() is yet to be implemented with how it will interact with mymalloc()

--- testing.c
+++ testing.c
@@ -3,6 +3,8 @@
 #include <stddef.h>
 #include "header.h"
 
+int free_cap = MEM_SIZE-sizeof(mem_block);
+
 void* MyMalloc(size_t size){
     mem_block *curr, *prev;
     void *output = NULL;
@@ -11,30 +13,32 @@ void* MyMalloc(size_t size){
         free_mem->size = 25000 - sizeof(mem_block);
         free_mem->free = 1;
         free_mem->next = NULL;  
-        printf("\nMemory Initialization Successful.");
+        printf("\n*** Memory Initialization Successful. Capacity : %d\n", MEM_SIZE-sizeof(mem_block));
     }
 
     curr = free_mem;
 
     while(curr){
-        printf("\tChecked\t");
         if(curr->size >= size && curr->free == 1){
             break;
         }
+        if(curr->next == NULL){
+            break;
+        } 
         curr = curr->next;
     }
 
-    if((curr->size < size || curr->free == 0) && (curr->next == NULL)){
-        printf("\nMemory Alloaction Unsuccessful. Not enough Memory in the heap.");
+    if(curr->size < size || curr->free == 0){
+        printf("\nMemory Alloaction Unsuccessful. Requested %d Bytes. %d Bytes remaining out of %d Bytes\n", size, free_cap,  MEM_SIZE-sizeof(mem_block));
         output = NULL;
         return output;
     }
     else if(curr->size == size){
         curr->free = 0;
         curr->size = size;
-        output = (void*)(curr + sizeof(mem_block));
-        printf("\nMemory Allocation Successful. Memory is full.");
-        printf("\nReturned Pointer with the address of : %x", output);
+        output = (void*)(curr + sizeof(mem_block));\
+        printf("\n%d Bytes Allocated. Memory is at Capacity", size);
+        printf("\nReturned Pointer with the address of : %x\n", output);
         return output;
     }
     else{
@@ -45,9 +49,10 @@ void* MyMalloc(size_t size){
         curr->size = size;
         curr->free = 0;
         curr->next = free_block;
+        free_cap -= size + sizeof(mem_block);
         output = (void*)(curr + sizeof(mem_block));
-        printf("\nMemory Allocation Successful.");
-        printf("\nReturned Pointer with the address of : %x", output);
+        printf("\n%d Bytes Allocated. %d Bytes remaining out of %d Bytes.", size, free_cap, MEM_SIZE-sizeof(mem_block));
+        printf("\nReturned Pointer with the address of : %x\n", output);
         return output;
     }
 }
@@ -57,6 +62,7 @@ int main(){
     MyMalloc(3000);
     MyMalloc(400);
     MyMalloc(2000);
+    MyMalloc(1504);
 
     return 0;
 }
